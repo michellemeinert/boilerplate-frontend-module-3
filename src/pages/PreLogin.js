19127ad86420ab2be5247bf3e2c16bfdb2fe4aca
michellemeinert <michelle_meinert@yahoo.de>
2019-05-29 10:27:46 +0200

    removed input border color

---
+++ src/pages/PreLogin.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class PreLogin extends Component {
+  render(){
+    return(
+      <div>
+        <p>
+          connectDev is a platform that promotes the collaboration of developers and designers on different open-source projects.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default PreLogin
